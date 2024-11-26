870620bdeb094311a009d708b0e90cf16e4f02cd
Hibah Abid <hibah28@live.co.uk>
2024-11-26 19:57:24 +0000

    loops in python

---
+++ session-two/loops.py
@@ -0,0 +1,15 @@
+# for loop
+print("Before the loop\n")
+
+for loop_number in range(5) :
+    print(f"We are in loop no: {loop_number}")
+
+print("\nAfter the loop")
+
+# for loop on a string and a pattern
+for i in "Hello":
+    print(i)
+
+# for loop on a list of items
+for var in ["apples", "bananas", "cherries"] :
+    print(var)
